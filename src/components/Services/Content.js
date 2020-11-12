25eaf06ddea492d578a3e99a092e852c157e1fd4
jsusilo <enr@bk.ru>
2020-11-13 00:33:01 +0500

    home services

---
+++ src/components/Services/Content.js
@@ -0,0 +1,151 @@
+import React from "react";
+
+import i from "../../assets/images/services-370x789.png";
+
+class Content extends React.Component {
+  render() {
+    return (
+      <section className="section section-md bg-default">
+        <div className="container">
+          <h3 className="wow fadeInLeft text-center">Services</h3>
+          <p className="wow fadeInRight text-center subtitle">
+            Sed in lacus ut enim adipiscing aliquet
+          </p>
+          <div className="row row-xl align-items-center row-30">
+            <div className="col-lg-4 col-md-6">
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".1s"
+              >
+                <span className="icon fa fa-coffee" />
+                <a href="#">Branding</a>
+                <p>
+                  Sunt torquises dignus fatalis, primus bullaes. Ridetis velox
+                  ducunt ad flavum genetrix. Cum spatii peregrinationes, omnes
+                  finises desiderium lotus, barbatus byssuses.
+                </p>
+              </div>
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".2s"
+              >
+                <span className="icon fa fa-laptop" />
+                <a href="#">Web Design</a>
+                <p>
+                  Hydra experimentums, tanquam azureus compater. Clabulares sunt
+                  eleatess de azureus candidatus. Varius, lotus accentors etiam
+                  attrahendam de rusticus, flavum mens.
+                </p>
+              </div>
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".3s"
+              >
+                <span className="icon fa fa-smile-o" />
+                <a href="#">Logo Design</a>
+                <p>
+                  Bromiums sunt cottas de varius castor. Palus, elevatus, et
+                  classis. Paluss velum! Cum solitudo cantare, omnes navises
+                  prensionem mirabilis, secundus ionicis tormentoes.
+                </p>
+              </div>
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".4s"
+              >
+                <span className="icon fa fa-rocket" />
+                <a href="#">Illustrations</a>
+                <p>
+                  Eheu, brodium! Poeta persuaderes, tanquam teres hilotae. Ausus
+                  fidelis medicina est. Cum visus cadunt, omnes fiscinaes
+                  carpseris audax, rusticus eposes.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4 d-none d-lg-block">
+              <div className="svg-device">
+                <svg
+                  id="svg2"
+                  xmlns="https://www.w3.org/2000/svg"
+                  xmlnsXlink="https://www.w3.org/1999/xlink"
+                  xmlSpace="preserve"
+                  version="1.1"
+                  x="0px"
+                  y="0px"
+                  viewBox="0 0 370 789"
+                >
+                  <defs>
+                    <style
+                      type="text/css"
+                      dangerouslySetInnerHTML={{
+                        __html:
+                          "\n                      #path3764 {\n                      \tstroke-dasharray: 2100;\n                      \tstroke-dashoffset: 0;\n                      \tstroke: transparent;\n                      \tfill: none;\n                      \t-moz-animation: 3s draw ease forwards, 1s hide-stroke ease forwards 3s;\n                      \t-o-animation: 3s draw ease forwards, 1s hide-stroke ease forwards 3s;\n                      \t-webkit-animation: 3s draw ease forwards, 1s hide-stroke ease forwards 3s;\n                      \tanimation: 3s draw ease forwards, 1s hide-stroke ease forwards 3s;\n                      }\n                      \n                      @-moz-keyframes draw {\n                      \tfrom {\n                      \t\tstroke-dashoffset: 2100;\n                      \t\tstroke: #000;\n                      \t}\n                      \tto {\n                      \t\tstroke-dashoffset: 0;\n                      \t\tstroke: #000;\n                      \t}\n                      }\n                      \n                      @-webkit-keyframes draw {\n                      \tfrom {\n                      \t\tstroke-dashoffset: 2100;\n                      \t\tstroke: #000;\n                      \t}\n                      \tto {\n                      \t\tstroke-dashoffset: 0;\n                      \t\tstroke: #000;\n                      \t}\n                      }\n                      \n                      @keyframes draw {\n                      \tfrom {\n                      \t\tstroke-dashoffset: 2100;\n                      \t\tstroke: #000;\n                      \t}\n                      \tto {\n                      \t\tstroke-dashoffset: 0;\n                      \t\tstroke: #000;\n                      \t}\n                      }\n                      \n                      @-moz-keyframes hide-stroke {\n                      \tfrom {\n                      \t\tstroke: #000;\n                      \t}\n                      \tto {\n                      \t\tstroke: transparent;\n                      \t}\n                      }\n                      \n                      @-webkit-keyframes hide-stroke {\n                      \tfrom {\n                      \t\tstroke: #000;\n                      \t}\n                      \tto {\n                      \t\tstroke: transparent;\n                      \t}\n                      }\n                      \n                      @keyframes hide-stroke {\n                      \tfrom {\n                      \t\tstroke: #000;\n                      \t}\n                      \tto {\n                      \t\tstroke: transparent;\n                      \t}\n                      }\n                    ",
+                      }}
+                    />
+                  </defs>
+                  <path
+                    id="path3764"
+                    d="m 250.09375,0.25 0,2.34375 L 62,2.59375 C 13.192817,5.8196142 5.6850321,40.641637 4.8125,54.375 l 0.0625,45.84375 -3.3125,0.0625 -0.96875,1 0,27.90625 0.9375,1.09375 3.46875,-0.0625 0,36.09375 -3.3686547,0.12627 -0.96874996,0.9375 0,20.9375 0.99999996,1 L 5.03125,189.1875 5,224.21875 l -3.3061547,0.12627 -1.03124996,1.34375 0,20.28125 1.03124996,0.875 3.3999047,-0.12627 -0.125,406.875 C 9.2159232,681.99608 25.03614,694.61136 46.375,699.75 l 281.53125,0 c 25.85828,-4.38704 39.55622,-25.21839 41.78125,-44.9375 l 0,-600.53125 C 368.71668,29.498588 350.29053,3.2261301 309.09375,3.71875 l 0,-3.46875 z M 185.75,33.53125 c 0.20265,-0.0075 0.40742,-0.01151 0.625,0 6.7399,-0.08159 6.375,9.22575 0,9.65625 -6.04974,-0.349908 -6.90701,-9.423703 -0.625,-9.65625 z m 31.1875,24.6875 c 4.12441,1.647425 4.47051,11.457495 0,12.9375 l -60.75,0 c -4.84418,-1.163175 -4.89703,-11.943397 0,-12.875 z M 343,104.1875 l 2.72179,500.03125 -314.523496,0 0,-500 312.282026,-0.0312 z"
+                  />
+                </svg>
+                <img src={i} alt />
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6">
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".1s"
+              >
+                <span className="icon fa fa-bullhorn" />
+                <a href="#">Development</a>
+                <p>
+                  Sunt torquises dignus fatalis, primus bullaes. Ridetis velox
+                  ducunt ad flavum genetrix. Cum spatii peregrinationes, omnes
+                  finises desiderium lotus, barbatus byssuses.
+                </p>
+              </div>
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".2s"
+              >
+                <span className="icon fa fa-camera" />
+                <a href="#">Photography</a>
+                <p>
+                  Hydra experimentums, tanquam azureus compater. Clabulares sunt
+                  eleatess de azureus candidatus. Varius, lotus accentors etiam
+                  attrahendam de rusticus, flavum mens.
+                </p>
+              </div>
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".3s"
+              >
+                <span className="icon fa fa-edit" />
+                <a href="#">Marketing</a>
+                <p>
+                  Bromiums sunt cottas de varius castor. Palus, elevatus, et
+                  classis. Paluss velum! Cum solitudo cantare, omnes navises
+                  prensionem mirabilis, secundus ionicis tormentoes.
+                </p>
+              </div>
+              <div
+                className="services-box-modern wow fadeInUp"
+                data-wow-delay=".4s"
+              >
+                <span className="icon fa fa-child" />
+                <a href="#">Support</a>
+                <p>
+                  Eheu, brodium! Poeta persuaderes, tanquam teres hilotae. Ausus
+                  fidelis medicina est. Cum visus cadunt, omnes fiscinaes
+                  carpseris audax, rusticus eposes.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Content;
