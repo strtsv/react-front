cab57df851723f302f87df2778e43ca0d46111fc
jsusilo <enr@bk.ru>
2020-11-16 00:04:06 +0500

    About OurTeam comp

---
+++ src/components/About/OurTeam.js
@@ -0,0 +1,237 @@
+import React from "react";
+
+import i1 from "../../assets/images/user-4-70x70.jpg";
+import i2 from "../../assets/images/user-5-70x70.jpg";
+import i3 from "../../assets/images/user-6-70x70.jpg";
+import i4 from "../../assets/images/user-7-70x70.jpg";
+import i5 from "../../assets/images/user-8-70x70.jpg";
+import i6 from "../../assets/images/user-9-70x70.jpg";
+
+class OurTeam extends React.Component {
+  render() {
+    return (
+      <section className="section section-md bg-dark-gray">
+        <div className="container">
+          <h3 className="text-white wow fadeInLeft text-center">Our Team</h3>
+          <p className="wow fadeInRight text-center subtitle">
+            Enim ad minim veniam, quis nostrud exerci tation ullamcorp
+          </p>
+          <div className="row row-xl row-50">
+            <div className="col-md-6">
+              <div className="profile-box">
+                <div className="profile-box-media">
+                  <img src={i1} alt width={70} height={70} />
+                </div>
+                <div className="profile-box-title">
+                  <span className="name">Monica Perry</span>
+                  <span className="position">veugiat nulla</span>
+                </div>
+                <p>
+                  Zeta cresceres, tanquam castus fuga. Pulchritudine de teres
+                  clinias, quaestio animalis! Nunquam contactus abnoba. Pol, a
+                  bene sensorem, placidus hydra! Superbus devirginatos ducunt ad
+                  brodium. Festus, alter omnias semper amor de brevis,
+                  domesticus canis.
+                </p>
+                <ul className="social-list">
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="profile-box">
+                <div className="profile-box-media">
+                  <img src={i2} alt width={70} height={70} />
+                </div>
+                <div className="profile-box-title">
+                  <span className="name">Eva Savits</span>
+                  <span className="position">veugiat nulla</span>
+                </div>
+                <p>
+                  Nunquam quaestio fluctui. Cum rumor studere, omnes verpaes
+                  talem bassus, festus compateres. Cliniass prarere in bi-color
+                  caelos! Est alter castor, cesaris. Impositio, musa, et
+                  itineris tramitem. Fortis, teres hilotaes unus carpseris de
+                  secundus, brevis tumultumque.
+                </p>
+                <ul className="social-list">
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="profile-box">
+                <div className="profile-box-media">
+                  <img src={i3} alt width={70} height={70} />
+                </div>
+                <div className="profile-box-title">
+                  <span className="name">Mark Johnson</span>
+                  <span className="position">veugiat nulla</span>
+                </div>
+                <p>
+                  Experimentum patienter ducunt ad albus contencio. Fluctui de
+                  noster capio, aperto uria! Nunquam desiderium ionicis
+                  tormento. Boreass accelerare in hamburgum! Eheu, detrius! Ubi
+                  est castus orgia? Cum lanista potus, omnes heureteses tractare
+                  castus, brevis demolitionees.
+                </p>
+                <ul className="social-list">
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="profile-box">
+                <div className="profile-box-media">
+                  <img src={i4} alt width={70} height={70} />
+                </div>
+                <div className="profile-box-title">
+                  <span className="name">Julia Berzkalna</span>
+                  <span className="position">veugiat nulla</span>
+                </div>
+                <p>
+                  Eheu, ferox fluctui! Verpas unda! Fortis messor cito reperires
+                  devatio est. A falsis, ausus rusticus fraticinida. Neuter,
+                  ferox lunas inciviliter imperium de barbatus, secundus lactea.
+                  Placidus, peritus cottas interdum amor de rusticus, talis
+                  accola.
+                </p>
+                <ul className="social-list">
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="profile-box">
+                <div className="profile-box-media">
+                  <img src={i5} alt width={70} height={70} />
+                </div>
+                <div className="profile-box-title">
+                  <span className="name">Camala Haddon</span>
+                  <span className="position">veugiat nulla</span>
+                </div>
+                <p>
+                  Cum coordinatae experimentum, omnes exsules reperire castus,
+                  noster onuses. Solems velum, tanquam dexter rumor. Heuretes,
+                  bromium, et coordinatae. Cur aonides peregrinatione? Cur
+                  solitudo messis? Cum bulla crescere, omnes exemplares fallere
+                  germanus, alter accentores.
+                </p>
+                <ul className="social-list">
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="profile-box">
+                <div className="profile-box-media">
+                  <img src={i6} alt width={70} height={70} />
+                </div>
+                <div className="profile-box-title">
+                  <span className="name">Monica Perry</span>
+                  <span className="position">veugiat nulla</span>
+                </div>
+                <p>
+                  Nunquam imperium victrix. Cur sensorem peregrinationes? Lamia
+                  camerarius visus est. Parmas congregabo in primus aboa!
+                  Medicinas assimilant, tanquam clemens urbs. Musas observare in
+                  emeritis virundum! Cum liberi volare, omnes lanistaes
+                  desiderium emeritis, azureus messores.
+                </p>
+                <ul className="social-list">
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="icon fa fa-instagram" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default OurTeam;
