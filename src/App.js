175b9bf700a93072c9a595e99010b63d27d3c5d4
jsusilo <enr@bk.ru>
2020-11-18 21:33:21 +0500

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import "./assets/css/bootstrap.css";
+import "./assets/css/fonts.css";
+import "./assets/css/style.css";
+
+import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
+// import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+// import CatalogList from "./pages/CatalogList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/services.html" component={Services} />
+      <Route exact path="/projects.html" component={Projects} />;
+      <Route exact path="/about.html" component={About} />
+      <Route exact path="/contacts.html" component={Contacts} />; return{" "}
+      <Route exact path="/" component={Home} />; return{" "}
+      <Route exact path="/index.html" component={Home} />;
+    </Switch>
   );
 }
 
